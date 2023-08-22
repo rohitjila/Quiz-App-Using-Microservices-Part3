03d2ce0e487a1d8b7c876cdeb311269178e42d0c
Rohit-Raju665 <codecodecode81@gmail.com>
2023-08-22 13:38:27 +0530

    creating service registry

---
+++ service-registry/src/test/java/com/rohit/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
